f7299d3fa2596403a773083f72315eadfba72e3e
Artur Novikov <artur.20177102@mail.ru>
2024-10-23 19:23:02 +0300

    bank-dash hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default {
   devtools: { enabled: true },
 
+  // Указываем статическую цель
+  target: "static", // Указывает на то, что проект будет статически сгенерирован
+
   build: {
     loaders: {
       scss: {
@@ -11,9 +14,17 @@ export default {
   },
 
   css: ["~/assets/main.scss"],
+
   modules: ["@element-plus/nuxt"],
+
   elementPlus: {
     importIcons: true, // Включает автоматический импорт иконок
   },
+
   compatibilityDate: "2024-07-11",
+
+  // Добавляем базовый путь роутинга, если вы размещаете сайт не на корневом уровне домена
+  router: {
+    base: "/", // Измените на '/<REPO_NAME>/' для GitHub Pages или оставьте '/' для Netlify
+  },
 };
